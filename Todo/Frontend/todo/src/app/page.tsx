7fa5fa3eb2fbdeb6e917efbbb3cc22ba46e989d1
Iori205 <b.sodbilegt11@gmail.com>
2025-09-30 09:00:48 +0800

    Backend

--- Todo/Frontend/todo/src/app/page.tsx
+++ Todo/Frontend/todo/src/app/page.tsx
@@ -1,4 +1,21 @@
-const TodoBackend = () => {
-  return <div></div>
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function TodoBackend() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }, []);
+  return (
+    <div>
+      {products.map((product) => (
+        <div key={product.id}>{product.name}</div>
+      ))}
+    </div>
+  );
 }
-export default TodoBackend
